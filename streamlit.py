67fccf7d26e400166da6a75d3fdf8e6ea86e9acf
Alfre619 <aq98vs619@gmail.com>
2024-04-20 17:12:10 -0600

    Updated histogram

--- streamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd  # Import pandas to fix the unresolved reference
-
-st.title('My First Streamlit App')
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
